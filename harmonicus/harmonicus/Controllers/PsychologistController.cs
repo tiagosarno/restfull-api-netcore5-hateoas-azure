983e463e99194a5041c566790f592e1e8f099285
Tiago Rocha Sarno <tiago.sarno@compartilhaclub.com.br>
2021-01-30 15:20:04 -0300

    Initializing Psychologists

---
+++ harmonicus/harmonicus/Controllers/PsychologistController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace harmonicus.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+        private readonly ILogger<CalculatorController> _logger;
+
+        public CalculatorController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult Get(string firstNumber, string secondNumber)
+        {
+            
+            return BadRequest("Invalid Input");
+        }
+    }
+}
---
+++ harmonicus/harmonicus/Model/Psychologist.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace harmonicus.Model
+{
+    public class Psychologist
+    {
+        public long id { get; set; }
+        public string FirstName { get; set; }
+        public long LastName { get; set; }
+        public long Address { get; set; }
+        public long Gender { get; set; }
+    }
+}
