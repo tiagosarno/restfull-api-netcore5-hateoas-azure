ca09e68a9e713fd6512bead1ed483315544bbc89
Tiago Rocha Sarno <tiago.sarno@compartilhaclub.com.br>
2021-03-14 22:22:09 -0300

    Implementando a estrutura de API para realizar upload de arquivos para o disco

---
+++ harmonicus/harmonicus/Business/IFileBusiness.cs
@@ -0,0 +1,14 @@
+﻿using harmonicus.Data.VO;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace harmonicus.Business
+{
+    public interface IFileBusiness
+    {
+        public byte[] GetFile(string filename);
+        public Task<FileDetailVO> SaveFileToDisk(IFormFile file);
+        public Task<List<FileDetailVO>> SaveFilesToDisk(IList<IFormFile> file);
+    }
+}
