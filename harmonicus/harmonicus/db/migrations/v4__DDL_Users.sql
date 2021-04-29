02b89bb36d10ece9348ef18ea1f130e789b9e148
tiagosarno <56081775+tiagosarno@users.noreply.github.com>
2021-04-29 09:53:58 -0300

    Delete v4__DDL_Users.sql

--- harmonicus/harmonicus/db/migrations/v4__DDL_Users.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `users` (
-	`id` bigint NOT NULL AUTO_INCREMENT,
-	`user_name` VARCHAR(50) NOT NULL DEFAULT '0',
-	`password` VARCHAR(130) NOT NULL DEFAULT '0',
-	`full_name` VARCHAR(120) NOT NULL,
-	`refresh_token` VARCHAR(500) NULL DEFAULT '0',
-	`refresh_token_expiry_time` DATETIME NULL DEFAULT NULL,
-	PRIMARY KEY (`id`)
-);
